b356480e44d99af6affbd68cfd42220be22e5586
FnxLucas <lucasvrxavier@gmail.com>
2026-06-02 16:30:53 -0400

    Revert "Merge branch 'main' into features"
    This reverts commit 2b22752c2f65940456c222f9ab5ebaacb34561f0, reversing
    changes made to 5931d2dda54a3a1584b8324abf21f583384d5016.

--- resources/views/itens/paginaItem.blade.php
+++ resources/views/itens/paginaItem.blade.php
@@ -1,22 +1,20 @@
 <x-layout titulo="{{$item->nome}}" textoLadoImagem="Detalhes do item" linkPagina="/itensperdidos">
-
+    
     <div class="ap_Card">
       <div class="ap_Col_Left">
         <div class="ap_Card_Image">
-          <img src="{{ asset('storage/'.$item->img) }}" alt="Foto do item perdido"/>
+          <img src="{{ asset('storage/'.$item->img) }}" alt="Item Perdido Imagem"/>
         </div>
+        <h1 class="ap_Item_Name">{{$item->nome}}</h1>
         <p class="ap_Description">
           {{$item->descricao}}
         </p>
+        <div class="card_Local">
+              <img src="{{asset('img/iconLocation.png')}}" alt="Localização" class="iconLocation">
+              <p> {{$item->localizacao}}</p>
+        </div>  
+        <a type="button" class="ap_Btn_Found" href="">Eu achei este item!</a>
       </div>
- 
-      <div class="ap_Col_Right">
-        <h1 class="ap_Item_Name">{{$item->nome}}</h1>
-        <button class="ap_Btn_Found">Esse item é meu!</button>
-      </div>
- 
     </div>
- 
-  </div>
 
 </x-layout>
